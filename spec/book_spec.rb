b646f657c17ed43734c87653ff3379df6a0afa4b
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-26 10:15:12 +0300

    Spec for Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../app/book'
+
+describe Book do
+  describe 'new instance of class Book' do
+    subject(:fail_book) { Book.new }
+    subject(:book) { Book.new('Test Book', 'Anonymous') }
+
+    it 'should raise an ArgumentError error if arguments not passed' do
+      expect {fail_book}.to raise_error(ArgumentError)
+    end
+
+    it 'instance of class Book' do
+      expect(book).to be_an_instance_of(Book)
+    end
+  end
+end
