12ad61538b698ffb55fe234e779d196971170fca
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-26 16:34:40 +0300

    Define Library#initialize

--- app/library.rb
+++ app/library.rb
@@ -1,3 +1,15 @@
-class Library
+require_relative 'author'
+require_relative 'book'
+require_relative 'order'
+require_relative 'reader'
 
+class Library
+  attr_writer :authors, :books, :oders, :readers
+
+  def initialize(authors: [], books: [], orders: [], readers: [], **args)
+    @authors = authors
+    @books = books
+    @orders = orders
+    @readers = readers
+  end
 end
