9d93c849712f1c828be3fe3c6836d37e702d861a
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-26 11:09:47 +0300

    Add additional tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../app/book'
 
 describe Book do
-  describe 'new instance of class Book' do
+  subject(:book) { Book.new('Test Book', 'Anonymous') }
+
+  describe '#new' do
     subject(:fail_book) { Book.new }
-    subject(:book) { Book.new('Test Book', 'Anonymous') }
 
     it 'should raise an ArgumentError error if arguments not passed' do
       expect {fail_book}.to raise_error(ArgumentError)
@@ -11,6 +12,18 @@ describe Book do
 
     it 'object should be an instance of Book class' do
       expect(book).to be_an_instance_of(Book)
+    end
+  end
+
+  describe '#title' do
+    it "should return book's title" do
+      expect(book.title).to eq('Test Book')
+    end
+  end
+
+  describe '#author' do
+    it 'should return author of the book' do
+      expect(book.author).to eq('Anonymous')
     end
   end
 end
