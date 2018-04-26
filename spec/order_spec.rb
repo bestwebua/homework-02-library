3a6ffdf705ceda9039e2b09e2793232f54a58ede
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-26 11:48:22 +0300

    Spec for Order class

---
+++ spec/order_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../app/order'
+
+describe Order do
+  subject(:order) { Order.new('Book', 'Reader', 'Date') }
+
+  describe '#new' do
+    subject(:fail_order) { Order.new }
+
+    it 'should raise an ArgumentError error if arguments not passed' do
+      expect {fail_order}.to raise_error(ArgumentError)
+    end
+
+    it 'object should be an instance of Order class' do
+      expect(order).to be_an_instance_of(Order)
+    end
+  end
+
+  describe '#book' do
+    it "should return book from the order" do
+      expect(order.book).to eq('Book')
+    end
+  end
+
+  describe '#reader' do
+    it 'should return reader from the order' do
+      expect(order.reader).to eq('Reader')
+    end
+  end
+
+  describe '#date' do
+    it 'should return date from the order' do
+      expect(order.date).to eq('Date')
+    end
+  end
+end
