f50989379f49c5ebe75746fb7da8ef2cbb4729e6
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-30 09:45:13 +0300

    Application runner sample

---
+++ run.rb
@@ -0,0 +1,23 @@
+require_relative 'app/library'
+
+library = Library.new
+
+library.add('author',
+            name: 'Paolo Perrotta',
+            biography: 'Awesome Paolo Perrotta bio...')
+
+library.add('book',
+            title: 'Metaprogramming Ruby',
+            author: library.authors.last)
+
+library.add('reader',
+            name: 'John Doe',
+            email: 'john_doe@domain.com',
+            city: 'City',
+            street: 'Street',
+            house: '42')
+
+library.add('order',
+            book: library.books.last,
+            reader: library.readers.last,
+            date: Time.new.strftime('%d.%m.%y'))
