45eb07c2bcfed78dde44b18ab0d84cb72bf10809
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-01 19:04:33 +0300

    Implement dynamical constructor for modules

--- app/constructor.rb
+++ app/constructor.rb
@@ -1,6 +1,6 @@
-def library_union_class_loader
+def library_union_loader
   path = "#{File.expand_path(File.dirname(__FILE__))}"
-  Dir["#{path}/classes/*.rb"].each { |file| require file }
+  Dir.glob("#{path}/*/*").each { |file| require file }
 end
 
 class LibraryUnionClass
@@ -19,26 +19,27 @@ class LibraryUnionClass
   end
 end
 
-library_union_class_loader
+library_union_loader
 
 class Constructor
   def self.build
-    path = "#{File.expand_path(File.dirname(__FILE__))}/classes"
-    Dir.new(path).entries
-  end
+    path = "#{File.expand_path(File.dirname(__FILE__))}"
+    files = Dir.glob("#{path}/*/*")
+    pattern = /.+\/app\/(.+)\/(.+).rb\z/
 
-  def self.files
-    @files ||= self.dir.select { |file| file[/(.+)\.rb/] }
+    files.group_by { |i| i[/#{pattern}/,1] }.map do |type, files|
+      [type, files.map { |file| file[/#{pattern}/,2] }]
+    end.to_h
   end
 
   def self.attributes
-    @attributes ||= self.files.map { |attribute| (attribute[/(.+)\./,1] + 's').to_sym }
+    @attributes ||= self.build['classes'].map { |attribute| (attribute + 's').to_sym }
   end
 
   def self.classes
     @classes ||=
-      self.files.map do |classe|
-        classe = classe[/(.+)\./,1].capitalize
+      self.build['classes'].map do |classe|
+        classe.capitalize!
         [classe, eval("#{classe}").attributes]
       end.to_h
   end
@@ -48,11 +49,7 @@ class Constructor
     @vars[target]
   end
 
-  class << self
-
-    protected
-    def dir
-      @dir ||= self.build
-    end
+  def self.modules
+    @modules ||= self.build['modules'].map(&:capitalize)
   end
 end
