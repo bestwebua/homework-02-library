da4a1f6119a506479936bbe27d80218f51a539ac
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-15 20:51:00 +0300

    Refactor #top_reader, #top_book, #top_orders_by

--- app/modules/analytics.rb
+++ app/modules/analytics.rb
@@ -1,25 +1,20 @@
  module Analytics
   def top_reader
-    get_first(top_orders_by(:reader))
+    top_orders_by(:reader).first
   end
 
   def top_book
-    get_first(top_orders_by(:book))
+    top_orders_by(:book).first
   end
 
   def count_readers_of_bestsellers_top3
-    top3 = top_orders_by(:book, 3)
-    top3.flatten.select { |object| object.is_a?(Order) }.uniq(&:reader).size
+    top_orders_by(:book, 3).flatten.select { |object| object.is_a?(Order) }.uniq(&:reader).size
   end
 
   private
 
   def top_orders_by(attribute, top_of=nil)
-    return nil if @orders.nil?
-    self.orders.group_by(&attribute).max_by(top_of) { |k,v| v.size }
-  end
-
-  def get_first(array)
-    array.nil? ? nil : array.first
+    return [] unless @orders
+    self.orders.group_by(&attribute).max_by(top_of) { |k,v| v.size } || []
   end
 end
